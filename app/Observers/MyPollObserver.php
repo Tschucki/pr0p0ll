446d792ce1ae43a891c6d994d4c8633e2146a98e
Marcel <marceldjnairo@gmail.com>
2024-01-09 23:35:01 +0100

    Add MyPoll Model Observer

---
+++ app/Observers/MyPollObserver.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Answer;
+use App\Models\Polls\MyPoll;
+use App\Models\User;
+use App\Notifications\PollNeedsReviewNotification;
+use Illuminate\Support\Facades\Notification;
+
+class MyPollObserver
+{
+    /**
+     * Handle the MyPoll "created" event.
+     */
+    public function created(MyPoll $myPoll): void
+    {
+        //
+    }
+
+    /**
+     * Handle the MyPoll "updated" event.
+     */
+    public function updated(MyPoll $myPoll): void
+    {
+        // Check if the poll has been turned in for review
+        if ($myPoll->isDirty('in_review') && $myPoll->isInReview()) {
+            // Send a notification to the admin
+            Notification::send(User::admin()->get(), new PollNeedsReviewNotification($myPoll));
+        }
+    }
+
+    /**
+     * Handle the MyPoll "deleted" event.
+     */
+    public function deleted(MyPoll $myPoll): void
+    {
+        //
+    }
+
+    /**
+     * Handle the MyPoll "restored" event.
+     */
+    public function restored(MyPoll $myPoll): void
+    {
+        //
+    }
+
+    /**
+     * Handle the MyPoll "force deleted" event.
+     */
+    public function forceDeleted(MyPoll $myPoll): void
+    {
+        //
+    }
+}
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Polls\MyPoll;
+use App\Observers\MyPollObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -28,7 +30,7 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        MyPoll::observe(MyPollObserver::class);
     }
 
     /**
