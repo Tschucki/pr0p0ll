66fce7c8fdb04a03cb57b2ebdcfd2c77cb75437f
Marcel <marceldjnairo@gmail.com>
2024-01-09 23:34:52 +0100

    Add PollNeedsReviewNotification

---
+++ app/Notifications/PollNeedsReviewNotification.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Abstracts\Poll as AbstractPoll;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+// TODO: Add ShouldQueue
+class PollNeedsReviewNotification extends Notification
+{
+    use Queueable;
+
+    private AbstractPoll $poll;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(AbstractPoll $poll)
+    {
+        $this->poll = $poll;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return [
+            'mail',
+            // TODO: Add Pr0gramm Channel
+        ];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->line('Hallo Admin,')
+            ->line('Eine Umfrage braucht eine Überprüfung.')
+            ->action('Zur Umfrage', url(route('filament.pr0p0ll.resources.all-polls.edit', ['record' => $this->poll->getKey()])))
+            ->line('Danke');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
