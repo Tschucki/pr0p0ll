0036062960923b7447f79c44812f658c2f99db99
Marcel <marceldjnairo@gmail.com>
2024-01-09 23:25:43 +0100

    Add guest Middleware to Login Routes

--- routes/web.php
+++ routes/web.php
@@ -3,26 +3,17 @@
 use App\Http\Controllers\Pr0authController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     // TODO: Add Landing
     return Redirect::route('filament.pr0p0ll.pages.dashboard');
 });
 
-Route::get('/oauth/callback', [Pr0authController::class, 'callback'])->name('oauth.callback');
+Route::middleware(['guest'])->group(function () {
+    Route::get('/oauth/callback', [Pr0authController::class, 'callback'])->name('oauth.callback');
 
-Route::get('/oauth/start', [Pr0authController::class, 'start'])->name('oauth.start');
+    Route::get('/oauth/start', [Pr0authController::class, 'start'])->name('oauth.start');
 
-Route::get('login', function () {
-    return Redirect::route('filament.pr0p0ll.auth.login');
-})->name('login');
+    Route::get('login', function () {
+        return Redirect::route('filament.pr0p0ll.auth.login');
+    })->name('login');
+});
