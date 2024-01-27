a23bc1972604adb88df4fc1648219360b9453847
Marcel <marceldjnairo@gmail.com>
2024-01-27 23:20:34 +0100

    Update UpdateUserData Page

--- app/Filament/Pages/UpdateUserData.php
+++ app/Filament/Pages/UpdateUserData.php
@@ -8,7 +8,13 @@ use App\Enums\Region;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Select;
 use Filament\Pages\Auth\EditProfile;
+use Filament\Support\Facades\FilamentView;
 use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use function Filament\Support\is_app_url;
 
 class UpdateUserData extends EditProfile
 {
@@ -46,7 +52,66 @@ class UpdateUserData extends EditProfile
 
     public function save(): void
     {
-        // TODO: Add own save as some props are hidden
-        dd('TODO');
+        try {
+            $this->callHook('beforeValidate');
+
+            $data = $this->form->getState();
+
+            $this->callHook('afterValidate');
+
+            $data = $this->mutateFormDataBeforeSave($data);
+
+            $this->callHook('beforeSave');
+
+            $this->handleRecordUpdate($this->getUser(), $data);
+
+            $this->callHook('afterSave');
+        } catch (\Throwable $throwable) {
+            return;
+        }
+
+        $this->getSavedNotification()?->send();
+
+        if ($redirectUrl = $this->getRedirectUrl()) {
+            $this->redirect($redirectUrl, navigate: FilamentView::hasSpaMode() && is_app_url($redirectUrl));
+        }
+    }
+
+    protected function handleRecordUpdate(Model $record, array $data): Model
+    {
+        Validator::make($this->data, [
+            'gender' => 'nullable|in:M,F',
+            'birthday' => 'nullable|date',
+            'nationality' => ['nullable', Rule::in(Nationality::cases())],
+            'region' => ['nullable', Rule::in(Region::cases())],
+        ]);
+
+        $record->update($data);
+
+        return $record;
+    }
+
+
+    /**
+     * @throws \Exception
+     */
+    protected function fillForm(): void
+    {
+        $user = $this->getUser();
+
+        $data = [
+            'gender' => $user->gender,
+            'birthday' => $user->birthday ? Carbon::parse($user->birthday)->format('d.m.Y') : null,
+            'nationality' => $user->nationality,
+            'region' => $user->region,
+        ];
+
+        $this->callHook('beforeFill');
+
+        $data = $this->mutateFormDataBeforeFill($data);
+
+        $this->form->fill($data);
+
+        $this->callHook('afterFill');
     }
 }
