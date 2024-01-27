1371a2411e810c864dd1e9b8a62239860574b860
Marcel <marceldjnairo@gmail.com>
2024-01-27 16:20:13 +0100

    Limit ClosesAfter to 6 weeks

--- app/Enums/ClosesAfter.php
+++ app/Enums/ClosesAfter.php
@@ -10,11 +10,7 @@ enum ClosesAfter: string implements HasLabel
     case ONEWEEK = '+1 week';
     case TWOWEEKS = '+2 weeks';
     case THREEWEEKS = '+3 weeks';
-    case ONEMONTH = '+1 month';
-    case TWOMONTHS = '+2 months';
-    case THREEMONTHS = '+3 months';
-    case SIXMONTHS = '+6 months';
-    case ONEYEAR = '+1 year';
+    case SIXWEEKS = '+6 weeks';
 
     public function getLabel(): ?string
     {
@@ -23,11 +19,7 @@ enum ClosesAfter: string implements HasLabel
             self::ONEWEEK => 'Nach 1 Woche',
             self::TWOWEEKS => 'Nach 2 Wochen',
             self::THREEWEEKS => 'Nach 3 Wochen',
-            self::ONEMONTH => 'Nach 1 Monat',
-            self::TWOMONTHS => 'Nach 2 Monaten',
-            self::THREEMONTHS => 'Nach 3 Monaten',
-            self::SIXMONTHS => 'Nach 6 Monaten',
-            self::ONEYEAR => 'Nach 1 Jahr',
+            self::SIXWEEKS => 'Nach 6 Wochen',
         };
     }
 }
