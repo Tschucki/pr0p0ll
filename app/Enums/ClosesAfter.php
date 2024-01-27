cf6275b3ff02bda9cb9e474c9a4b59e6cf58d364
Marcel <marceldjnairo@gmail.com>
2024-01-27 18:21:41 +0100

    Update ClosesAfter

--- app/Enums/ClosesAfter.php
+++ app/Enums/ClosesAfter.php
@@ -10,6 +10,8 @@ enum ClosesAfter: string implements HasLabel
     case ONEWEEK = '+1 week';
     case TWOWEEKS = '+2 weeks';
     case THREEWEEKS = '+3 weeks';
+    case FOURWEEKS = '+4 weeks';
+    case FIVEWEEKS = '+5 weeks';
     case SIXWEEKS = '+6 weeks';
 
     public function getLabel(): ?string
@@ -19,6 +21,8 @@ enum ClosesAfter: string implements HasLabel
             self::ONEWEEK => 'Nach 1 Woche',
             self::TWOWEEKS => 'Nach 2 Wochen',
             self::THREEWEEKS => 'Nach 3 Wochen',
+            self::FOURWEEKS => 'Nach 4 Wochen',
+            self::FIVEWEEKS => 'Nach 5 Wochen',
             self::SIXWEEKS => 'Nach 6 Wochen',
         };
     }
