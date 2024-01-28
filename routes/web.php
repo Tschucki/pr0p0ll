a7316b430a3e16d4f5c642e69e802ea9cfcf1644
Marcel <marceldjnairo@gmail.com>
2024-01-28 13:38:59 +0100

    Use OAuth Login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\Pr0authController;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\Frontend\FrontendController::class, 'landing'])->name('frontend.landing');
@@ -15,10 +14,6 @@ Route::middleware(['guest'])->group(function () {
     Route::get('/oauth/start', [Pr0authController::class, 'start'])->name('oauth.start');
 
     Route::get('login', function () {
-        Auth::login(User::find(1));
-
-        return Redirect::route('frontend.landing');
-
         return Redirect::route('filament.pr0p0ll.auth.login');
     })->name('login');
 });
