b99c9b7837bf456f8891418c609c00c2d55cb560
Marcel <marceldjnairo@gmail.com>
2024-02-18 20:19:26 +0100

    Update Pr0 Post Creator

--- resources/views/filament/pr0post/creator/layouts/default.blade.php
+++ resources/views/filament/pr0post/creator/layouts/default.blade.php
@@ -5,7 +5,7 @@
 @endphp
 
 <div class="bg-[#161618] py-2 fi-section rounded-xl shadow-sm ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10"
-     id="pr0post">
+    id="pr0post">
     <h1 class="text-center text-3xl font-medium" @if($data['color'])style="color: {{$data['color']}}"@endif>pr0p0ll
         Umfrageauswertung</h1>
     <h1 class="text-center text-3xl mt-2 font-medium">Thema: <span
@@ -14,7 +14,7 @@
         <p class="text-center text-xl mt-2 font-medium">{{$data['description']}}</span></p>
     @endif
 
-    <div class="space-y-3 mt-6">
+    <div class="space-y-3 mt-6 w-full">
         @foreach($questions as $key => $question)
             @php
                 $newKey = $key . $data['description_' . $question->properties['answerData']['questionId']] . $data['color']
@@ -28,28 +28,17 @@
             @endif
         @endforeach
     </div>
-    <div class="border-t border-white/10 p-4 grid grid-cols-2 gap-2">
-        <div class="grid grid-cols-2">
-            <div>Teilnehmer:</div>
-            <div>
-                {{ $this->participants }}
-            </div>
-
-        </div>
-        <div class="grid grid-cols-2">
-            <div>Fragen:</div>
-            <div>{{ count($questions) }}</div>
-        </div>
-        <div class="grid grid-cols-2">
-            <div>Umfrageende:</div>
-            <div>{{ \App\Enums\ClosesAfter::from($this->record->closes_after)->getLabel() }}</div>
-        </div>
-        <div class="grid grid-cols-2">
-            <div>ID:</div>
-            <div>{{$this->record->getKey()}}</div>
-        </div>
-
-
+    <div class="border-t border-white/10 p-4">
+        <table class="table-fixed w-full">
+            <tr>
+                <td class="text-center">Teilnehmer: {{ $this->participants }}</td>
+                <td class="text-center">Fragen: {{ count($questions) }}</td>
+            </tr>
+            <tr>
+                <td class="text-center">Umfrageende: {{ \App\Enums\ClosesAfter::from($this->record->closes_after)->getLabel() }}</td>
+                <td class="text-center">ID: {{$this->record->getKey()}}</td>
+            </tr>
+        </table>
     </div>
     <div class="border-t border-white/10 pt-2 px-2">
         <div class="flex items-center justify-center flex-col">
@@ -63,43 +52,44 @@
             </div>
         </div>
     </div>
-</div>
+    </div>
 
-<script src="https://html2canvas.hertzen.com/dist/html2canvas.js"></script>
+    <script src="https://html2canvas.hertzen.com/dist/html2canvas.js"></script>
 
-<script>
-    const delay = ms => new Promise(res => setTimeout(res, ms));
-    downloadImage = () => {
-        html2canvas(document.getElementById('pr0post'), {
-            scrollX: 0,
-            scrollY: 0,
-            windowWidth: 1052,
-            backgroundColor: '#161618',
-            logging: true,
-            useCORS: true,
-            onclone: function (document) {
-                const post = document.getElementById('pr0post')
-                post.style.backgroundColor = '#161618';
-                post.style.borderRadius = 0;
-                post.style.boxShadow = 'none';
-            }
-        }).then(function (canvas) {
-            let a = document.createElement('a');
-            a.href = canvas.toDataURL('image/png');
-            a.download = '{{$data['title']}}' + '.png';
-            a.click();
-        });
-    }
-</script>
+    <script>
+        const delay = ms => new Promise(res => setTimeout(res, ms));
+        downloadImage = () => {
+            html2canvas(document.getElementById('pr0post'), {
+                backgroundColor: '#161618',
+                logging: true,
+                useCORS: true,
+                onclone: function (document) {
+                    const post = document.getElementById('pr0post')
+                    post.style.backgroundColor = '#161618';
+                    post.style.borderRadius = 0;
+                    post.style.maxWidth = '1052px';
+                    post.style.boxShadow = 'none';
+                }
+            }).then(function (canvas) {
+                let a = document.createElement('a');
+                a.href = canvas.toDataURL('image/png');
+                a.download = '{{$data['title']}}' + '.png';
+                a.click();
+            });
+        }
+    </script>
 
-<style>
-    #pr0post .fi-section {
-        box-shadow: none;
-        background-color: #161618;
-    }
+    <style>
+        #pr0post .fi-section {
+            box-shadow: none;
+            background-color: #161618;
+        }
 
-    #pr0post {
-        background-color: #161618;
-    }
-</style>
+        #pr0post {
+            background-color: #161618;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+    </style>
 
