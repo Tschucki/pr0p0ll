59b4530e66bb731cb7b3bde074412e982a7c9651
Marcel <marceldjnairo@gmail.com>
2024-03-03 18:53:51 +0100

    Add Pulse

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,6 +14,7 @@ use App\Policies\PollPolicy;
 use App\Policies\PublicPollPolicy;
 use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -34,6 +35,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('viewPulse', static function (User $user) {
+            return $user->isAdmin();
+        });
     }
 }
