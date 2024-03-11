2d21ce5e0728519cd5ca0bb7cf005c9345946857
Marcel <marceldjnairo@gmail.com>
2024-03-11 22:31:32 +0100

    Update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,12 +6,16 @@ module.exports = {
     ],
     plugins: ['prettier'],
     rules: {
+        'vue/camelcase': ['error'],
+        'vue/require-v-for-key': ['error'],
         'vue/no-unused-properties': ['error'],
         'vue/no-v-html': 'off',
         'vue/multi-word-component-names': 'off',
         'prettier/prettier': ['error'],
         'vue/require-default-prop': 'off',
         'vue/singleline-html-element-content-newline': 0,
-        'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+        'vue/component-name-in-template-casing': ['error'],
+        'vue/attribute-hyphenation': 'off',
+        'vue/no-multi-spaces': ['error'],
     },
 }
