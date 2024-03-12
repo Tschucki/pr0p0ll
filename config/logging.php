dc9f77cfd319b8ac0828ae68ddcbe69f9454c42e
Marcel <marceldjnairo@gmail.com>
2024-03-12 23:02:26 +0100

    Change logging channel to daily

--- config/logging.php
+++ config/logging.php
@@ -56,7 +56,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['daily'],
             'ignore_exceptions' => false,
         ],
 
