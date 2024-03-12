96f6305523a0f400bc395b59ee2d9bd20b5355b3
Marcel <marceldjnairo@gmail.com>
2024-03-13 00:16:33 +0100

    Add FAQ

---
+++ app/Filament/Pages/FAQ.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Concerns\InteractsWithInfolists;
+use Filament\Infolists\Infolist;
+use Filament\Pages\Page;
+
+class FAQ extends Page
+{
+    use InteractsWithInfolists;
+    use InteractsWithForms;
+
+    protected static ?string $navigationIcon = 'heroicon-o-question-mark-circle';
+
+    protected static ?string $title = 'FAQ';
+
+    protected static ?string $navigationGroup = 'Hilfe';
+    protected static ?int $navigationSort = 200;
+
+    protected static ?string $slug = 'faq';
+
+    protected static string $view = 'filament.pages.f-a-q';
+
+    public function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                Section::make('WIP')->schema([
+                ])->collapsible(),
+                Section::make('Wie lege ich eine Umfrage an?')->schema([
+                ])->collapsible(),
+                Section::make('Wie lösche ich meinen Account?')->schema([
+                ])->collapsible(),
+                Section::make('Wann kann ich einen pr0gramm-Post erstellen?')->schema([
+                ])->collapsible()
+            ]);
+    }
+}
