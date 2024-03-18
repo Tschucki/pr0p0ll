75375f4c5834baef7eeb1de907abe156131a9a1e
Marcel <marceldjnairo@gmail.com>
2024-03-18 18:38:31 +0100

    Update DatabaseSeeder for prod

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             QuestionTypeSeeder::class,
-            UserSeeder::class,
-            PollSeeder::class,
+            //UserSeeder::class,
+            //PollSeeder::class,
             CategorySeeder::class,
             NotificationChannelSeeder::class,
             NotificationTypeSeeder::class,
