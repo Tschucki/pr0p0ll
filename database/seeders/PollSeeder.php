89bc627e4e011b3a9b68320d43f1e266569f7293
Marcel <marceldjnairo@gmail.com>
2024-03-20 13:56:52 +0100

    Update PollSeeder

--- database/seeders/PollSeeder.php
+++ database/seeders/PollSeeder.php
@@ -8,6 +8,7 @@ use App\Enums\QuestionType;
 use App\Models\AnswerTypes\BoolAnswer;
 use App\Models\AnswerTypes\MultipleChoiceAnswer;
 use App\Models\AnswerTypes\SingleOptionAnswer;
+use App\Models\AnswerTypes\TextAnswer;
 use App\Models\Polls\MyPoll;
 use App\Models\Question;
 use App\Models\User;
@@ -37,6 +38,41 @@ class PollSeeder extends Seeder
             $this->createQuestionsForPoll($myPoll);
             $this->createAnswersForPoll($myPoll);
         }
+        foreach (range(1, 1) as $poll) {
+            $myPoll = MyPoll::create([
+                'title' => 'My Poll',
+                'description' => 'This is my poll',
+                'user_id' => 1,
+                'closes_after' => '+3 days',
+                'closes_at' => now()->subDay(),
+                'published_at' => now()->subDays(4),
+                'admin_notes' => 'Eine klasse Umfrage',
+                'in_review' => false,
+                'approved' => true,
+                'visible_to_public' => true,
+                'not_anonymous' => true,
+            ]);
+
+            $this->createQuestionsForPoll($myPoll);
+            $this->createAnswersForPoll($myPoll);
+        }
+        foreach (range(1, 1) as $poll) {
+            $myPoll = MyPoll::create([
+                'title' => 'My Poll',
+                'description' => 'This is my poll',
+                'user_id' => User::inRandomOrder()->first()->getKey(),
+                'closes_after' => '+3 days',
+                'published_at' => now(),
+                'admin_notes' => 'Eine klasse Umfrage',
+                'in_review' => false,
+                'approved' => true,
+                'visible_to_public' => true,
+                'not_anonymous' => true,
+            ]);
+
+            $this->createQuestionsForPoll($myPoll);
+            $this->createAnswersForPoll($myPoll);
+        }
     }
 
     private function createQuestionsForPoll(MyPoll $myPoll): void
@@ -48,6 +84,7 @@ class PollSeeder extends Seeder
             $this->createSingleOptionQuestionForPoll($myPoll);
             $this->createBoolQuestionForPoll($myPoll);
             $this->createMultipleOptionQuestionForPoll($myPoll);
+            $this->createTextQuestionForPoll($myPoll);
         }
     }
 
@@ -84,6 +121,15 @@ class PollSeeder extends Seeder
         ]);
     }
 
+    private function createTextQuestionForPoll(MyPoll $myPoll): void
+    {
+        $myPoll->questions()->create([
+            'title' => 'Text Question',
+            'question_type_id' => \App\Models\QuestionType::where('component', QuestionType::TEXT)->firstOrFail()->getKey(),
+            'position' => 1,
+        ]);
+    }
+
     private function createAnswersForPoll(MyPoll $myPoll): void
     {
         $myPoll->questions->each(function (Question $question) use ($myPoll) {
@@ -93,8 +139,30 @@ class PollSeeder extends Seeder
                 $this->createMultipleOptionAnswersForQuestion($question, $myPoll);
             } elseif ($question->answerType() instanceof BoolAnswer) {
                 $this->createBoolAnswersForQuestion($question, $myPoll);
+            } elseif ($question->answerType() instanceof TextAnswer) {
+                $this->createTextAnswersForQuestion($question, $myPoll);
             }
         });
+    }
+
+    private function createTextAnswersForQuestion(Question $question, MyPoll $myPoll): void
+    {
+
+        foreach (range(1, 10) as $i) {
+            $user = User::inRandomOrder()->first();
+            $anonymousUser = $user->createAnonymousUser();
+            $anonymousUserId = $anonymousUser->getKey();
+            $question->answers()->create([
+                'answerable_id' => $question->answerType()->create([
+                    'answer_value' => 'AnswerText '.random_int(1, 3),
+                ])->getKey(),
+                'answerable_type' => get_class($question->answerType()),
+                'user_id' => null,
+                'poll_id' => $myPoll->getKey(),
+                'anonymous_user_id' => $anonymousUserId,
+            ]);
+        }
+        $myPoll->participants()->attach($user->getKey());
     }
 
     private function createSingleOptionAnswersForQuestion(Question $question, MyPoll $myPoll): void
