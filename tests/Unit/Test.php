bde62ecfa6e84f3b83cf32a7214fa6201c6191e4
Marcel <marceldjnairo@gmail.com>
2024-03-20 11:47:00 +0100

    Fix Unit Test

--- tests/Unit/Test.php
+++ tests/Unit/Test.php
@@ -6,6 +6,7 @@ use App\Models\User;
 
 it('returns demographic data as array', function () {
     \Illuminate\Support\Facades\Artisan::call('db:seed');
+    \Illuminate\Support\Facades\Artisan::call('db:seed UserSeeder');
     $data = User::first()->getDemographicData();
     $this->assertIsArray($data);
 });
