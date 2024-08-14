2de4d84691427aaad851a2c518ccf344c24a8c05
Marcel <marceldjnairo@gmail.com>
2024-08-14 21:56:34 +0200

    use accessor method as properties are now protected...

--- resources/views/filament/pr0post/creator/layouts/default.blade.php
+++ resources/views/filament/pr0post/creator/layouts/default.blade.php
@@ -1,15 +1,16 @@
 @php
-    $data = $this->data;
+    use App\Enums\ClosesAfter;$data = $this->data;
     $color = $data['color'];
     $questions = $this->getResults();
 @endphp
 
-<div class="bg-[#161618] py-2 fi-section rounded-xl shadow-sm ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10"
+<div
+    class="bg-[#161618] py-2 fi-section rounded-xl shadow-sm ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10"
     id="pr0post">
     <h1 class="text-center text-3xl font-medium" @if($data['color'])style="color: {{$data['color']}}"@endif>pr0p0ll
         Umfrageauswertung</h1>
     <h1 class="text-center text-3xl mt-2 font-medium">Thema: <span
-                @if($data['color'])style="color: {{$data['color']}}"@endif>{{$data['title']}}</span></h1>
+            @if($data['color'])style="color: {{$data['color']}}"@endif>{{$data['title']}}</span></h1>
     @if($data['description'])
         <p class="text-center text-xl mt-2 font-medium">{{$data['description']}}</span></p>
     @endif
@@ -17,12 +18,12 @@
     <div class="space-y-3 mt-6 w-full">
         @foreach($questions as $key => $question)
             @php
-                $newKey = $key . $data['description_' . $question->properties['answerData']['questionId']] . $data['color'] . now()->timestamp;
+                $newKey = $key . $data['description_' . $question->getProperties()['answerData']['questionId']] . $data['color'] . now()->timestamp;
             @endphp
-            @if($data['display_' . $question->properties['answerData']['questionId']])
+            @if($data['display_' . $question->getProperties()['answerData']['questionId']])
                 @livewire($question->widget, [
-                    'subHeading' => $data['description_' . $question->properties['answerData']['questionId']],
-                    'answerData' => $question->properties['answerData'],
+                    'subHeading' => $data['description_' . $question->getProperties()['answerData']['questionId']],
+                    'answerData' => $question->getProperties()['answerData'],
                     'color' => $data['color'],
                     'data' => $data,
                 ], key($newKey))
@@ -36,7 +37,8 @@
                 <td class="text-center">Fragen: {{ count($questions) }}</td>
             </tr>
             <tr>
-                <td class="text-center">Umfrageende: {{ \App\Enums\ClosesAfter::from($this->record->closes_after)->getLabel() }}</td>
+                <td class="text-center">
+                    Umfrageende: {{ ClosesAfter::from($this->record->closes_after)->getLabel() }}</td>
                 <td class="text-center">ID: {{$this->record->getKey()}}</td>
             </tr>
         </table>
@@ -53,44 +55,44 @@
             </div>
         </div>
     </div>
-    </div>
+</div>
 
-    <script src="https://html2canvas.hertzen.com/dist/html2canvas.js"></script>
+<script src="https://html2canvas.hertzen.com/dist/html2canvas.js"></script>
 
-    <script>
-        const delay = ms => new Promise(res => setTimeout(res, ms));
-        downloadImage = () => {
-            html2canvas(document.getElementById('pr0post'), {
-                backgroundColor: '#161618',
-                logging: true,
-                useCORS: true,
-                onclone: function (document) {
-                    const post = document.getElementById('pr0post')
-                    post.style.backgroundColor = '#161618';
-                    post.style.borderRadius = 0;
-                    post.style.maxWidth = '1052px';
-                    post.style.boxShadow = 'none';
-                }
-            }).then(function (canvas) {
-                let a = document.createElement('a');
-                a.href = canvas.toDataURL('image/png');
-                a.download = '{{$data['title']}}' + '.png';
-                a.click();
-            });
-        }
-    </script>
+<script>
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+    downloadImage = () => {
+        html2canvas(document.getElementById('pr0post'), {
+            backgroundColor: '#161618',
+            logging: true,
+            useCORS: true,
+            onclone: function(document) {
+                const post = document.getElementById('pr0post')
+                post.style.backgroundColor = '#161618';
+                post.style.borderRadius = 0;
+                post.style.maxWidth = '1052px';
+                post.style.boxShadow = 'none';
+            }
+        }).then(function(canvas) {
+            let a = document.createElement('a');
+            a.href = canvas.toDataURL('image/png');
+            a.download = '{{$data['title']}}' + '.png';
+            a.click();
+        });
+    }
+</script>
 
-    <style>
-        #pr0post .fi-section {
-            box-shadow: none;
-            background-color: #161618;
-        }
+<style>
+    #pr0post .fi-section {
+        box-shadow: none;
+        background-color: #161618;
+    }
 
-        #pr0post {
-            background-color: #161618;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
-    </style>
+    #pr0post {
+        background-color: #161618;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
 
