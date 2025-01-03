46e6c00447b54e3545a042fa41b24450c340b8e9
Marcel <marceldjnairo@gmail.com>
2025-01-03 16:39:38 +0100

    Update Leaderboard

--- app/Filament/Pages/Leaderboard.php
+++ app/Filament/Pages/Leaderboard.php
@@ -10,6 +10,7 @@ use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Table;
+use stdClass;
 
 class Leaderboard extends Page implements HasTable
 {
@@ -22,11 +23,35 @@ class Leaderboard extends Page implements HasTable
     public function table(Table $table): Table
     {
         return $table
-            ->query(User::query()->select('id', 'name'))
+            ->description(function (HasTable $livewire) {
+                // go through all the records and find the current auth user and show the position
+                $page = 1;
+                $users = User::withCount(['participations', 'approvedPolls'])
+                    ->orderByDesc('participations_count')
+                    ->orderByDesc('approved_polls_count')
+                    ->get(['id', 'name']);
+
+                // get the position of the current user
+                $position = $users->search(static function ($user) {
+                    return $user->id === auth()->id();
+                });
+
+
+                if ($position !== false) {
+                    $position++;
+                } else {
+                    $position = 'N/A';
+                }
+
+                return "Du bist auf Platz {$position} von {$users->count()} Nutzern.";
+            })
+            ->query(User::query()->withCount(['participations', 'approvedPolls'])
+                ->orderByDesc('participations_count')
+                ->orderByDesc('approved_polls_count'))
             ->defaultSort('participations_count', 'desc')
             ->columns([
                 TextColumn::make('position')->badge()->label('Position')->prefix('#')->state(
-                    static function (HasTable $livewire, \stdClass $rowLoop): string {
+                    static function (HasTable $livewire, stdClass $rowLoop): string {
                         return (string) (
                             $rowLoop->iteration +
                             ($livewire->getTableRecordsPerPage() * (
